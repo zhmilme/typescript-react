721eff71e34e50a6cd6589722002f076f2e466c0
zhmilme <157293813+zhmilme@users.noreply.github.com>
2024-01-27 18:19:29 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -126,6 +126,9 @@ class JSON_constructor extends React.Component {
                           }
                         }}
                         >
+                        <MenuItem disabled value="">
+                          <em>Placeholder</em>
+                        </MenuItem>
                         <MenuItem value={10}>Ten</MenuItem>
                         <MenuItem value={20}>Twenty</MenuItem>
                         <MenuItem value={30}>Thirty</MenuItem>
