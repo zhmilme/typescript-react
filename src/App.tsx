2afff60eef15250ae695c5199b0bf9e26eaf5bc0
zhmilme <157293813+zhmilme@users.noreply.github.com>
2024-01-27 17:25:30 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -88,6 +88,7 @@ class JSON_constructor extends React.Component {
   //[age, setAge] = this.useState('');
 
   handleChange = (event: SelectChangeEvent) => {
+        this.setState({ age: event.target.value, set: false });
         this.setState({ age: event.target.value, set: true });
       };
 
