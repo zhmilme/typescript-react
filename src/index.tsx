2b924c09a2011b11b81b2d7361462b482fd333be
zhmilme <157293813+zhmilme@users.noreply.github.com>
2024-02-08 14:16:40 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Screen from "./screen";
 import App from "./transg";
-import useDraggable, { handleOverlapCheck } from "./useDraggable";
+import useDraggable from "./useDraggable";
 import { Root, Main, Collapsable } from "./components/items";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
